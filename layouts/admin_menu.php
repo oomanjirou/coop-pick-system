b6e653eed91cd7aa37eec872096c941586789db8
Serena Ng Yen Xin <yenxinng@gmail.com>
2025-03-24 22:37:01 +0800

    first commit

---
+++ layouts/admin_menu.php
@@ -0,0 +1,50 @@
+<ul>
+  <li>
+    <a href="admin.php">
+      <i class="glyphicon glyphicon-home"></i>
+      <span>Dashboard</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-user"></i>
+      <span>User Management</span>
+    </a>
+    <ul class="nav submenu">
+      <li><a href="group.php">Manage Groups</a> </li>
+      <li><a href="users.php">Manage Users</a> </li>
+   </ul>
+  </li>
+  <li>
+    <a href="categorie.php" >
+      <i class="glyphicon glyphicon-indent-left"></i>
+      <span>Categories</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-th-large"></i>
+      <span>Products</span>
+    </a>
+    <ul class="nav submenu">
+       <li><a href="product.php">Manage Products</a> </li>
+       <li><a href="add_product.php">Add Products</a> </li>
+   </ul>
+  </li>
+  <li>
+    <a href="media.php" >
+      <i class="glyphicon glyphicon-picture"></i>
+      <span>Media Files</span>
+    </a>
+  </li>
+
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-duplicate"></i>
+       <span>Inventory Report</span>
+      </a>
+      <ul class="nav submenu">
+        <li><a href="inventory_report.php">Inventory by date </a></li>
+      </ul>
+  </li>
+</ul>
---
+++ layouts/user_menu.php
@@ -0,0 +1,36 @@
+<ul>
+  <li>
+    <a href="home.php">
+      <i class="glyphicon glyphicon-home"></i>
+      <span>Dashboard</span>
+    </a>
+  </li>
+
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-th-large"></i>
+      <span>Products</span>
+    </a>
+    <ul class="nav submenu">
+       <li><a href="product.php">Manage product</a> </li>
+       <li><a href="add_product.php">Add product</a> </li>
+   </ul>
+  </li>
+
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-signal"></i>
+       <span>Inventory Report</span>
+      </a>
+      <ul class="nav submenu">
+        <li><a href="inventory_report.php">Inventory by date </a></li>
+      </ul>
+  </li>
+
+  <li>
+    <a href="media.php" >
+      <i class="glyphicon glyphicon-picture"></i>
+      <span>Media</span>
+    </a>
+  </li>
+</ul>
